a9d0de20b0bd25f576c81ff50e088bc7721b0022
Jake Ascher <ascherj@gmail.com>
2025-06-17 11:53:06 -0600

    Complete 05-complexity

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,10 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSoFar = -Infinity;
+  for (let i = 0; i < arr.length - k; i++) {
+    const currentMax = arr.slice(i, i + k).reduce((sum, num) => sum + num);
+    maxSoFar = Math.max(maxSoFar, currentMax);
+  }
+  return maxSoFar;
+}
 
 module.exports = maxSubarraySum;
--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,18 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSoFar = -Infinity;
+  let windowSum = 0;
+  for (let i = 0; i < k; i++) {
+    windowSum += arr[i];
+  }
+
+  maxSoFar = windowSum;
+
+  for (let i = k; i < arr.length; i++) {
+    windowSum = windowSum - arr[i - k] + arr[i];
+    maxSoFar = Math.max(windowSum, maxSoFar);
+  }
+
+  return maxSoFar;
+}
 
 module.exports = maxSubarraySum;
